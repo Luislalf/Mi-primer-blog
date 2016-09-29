4b4328e5d7b79ebfd3ec76b144929f2a3af57eec
luis <luislopez.lalf@gmail.com>
2016-09-29 15:28:27 -0600

    Se ha cambiado el HTML par la página.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from blog.models import Articulo
+from django.utils import timezone
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+def articulo_lista(request):
+    articulos = Articulo.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    return render(request, 'blog/articulo_lista.html', {'articulos':articulos})
